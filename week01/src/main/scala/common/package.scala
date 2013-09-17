eaa454cad7f827d5dd3b9efdfd81fe77c3797f84
Toby Weston <toby.weston@gmail.com>
2013-09-17 19:28:49 +0100

    reformatting

--- week01/src/main/scala/common/package.scala
+++ week01/src/main/scala/common/package.scala
@@ -3,21 +3,21 @@ import java.io.File
 package object common {
 
   /** An alias for the `Nothing` type.
-   *  Denotes that the type should be filled in.
-   */
+    * Denotes that the type should be filled in.
+    */
   type ??? = Nothing
 
   /** An alias for the `Any` type.
-   *  Denotes that the type should be filled in.
-   */
+    * Denotes that the type should be filled in.
+    */
   type *** = Any
 
-  
+
   /**
    * Get a child of a file. For example,
-   * 
-   *   subFile(homeDir, "b", "c")
-   * 
+   *
+   * subFile(homeDir, "b", "c")
+   *
    * corresponds to ~/b/c
    */
   def subFile(file: File, children: String*) = {
--- week01/src/main/scala/recfun/Main.scala
+++ week01/src/main/scala/recfun/Main.scala
@@ -1,7 +1,9 @@
 package recfun
+
 import common._
 
 object Main {
+
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
