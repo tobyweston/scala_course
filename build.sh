ccad2e718ba1625fda38ed844a7d60eef9539aca
tobyweston <toby.weston@gmail.com>
2013-09-24 09:24:32 +0100

    adding travis CI build config. Uncomment each weeks build as you go

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#cd $CI_HOME/example
+#sbt test
+#cd $CI_HOME/week01
+#sbt test
+#cd $CI_HOME/week02
+#sbt test
+#cd $CI_HOME/week03
+#sbt test
+#cd $CI_HOME/week04
+#sbt test
+#cd $CI_HOME/week05
+#sbt test
+#cd $CI_HOME/week06
+#sbt test
+#cd $CI_HOME/week07
+#sbt test
