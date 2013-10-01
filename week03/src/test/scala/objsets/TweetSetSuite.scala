c5b225e387e739d793906f66c51e257dd24bcfe5
tobyweston <toby.weston@gmail.com>
2013-10-01 08:25:08 +0100

    tidy up test data

--- week03/src/test/scala/objsets/TweetSetSuite.scala
+++ week03/src/test/scala/objsets/TweetSetSuite.scala
@@ -9,16 +9,16 @@ import org.scalatest.junit.JUnitRunner
 class TweetSetSuite extends FunSuite {
   trait TestSets {
     val A = Tweet("a", "a body", 20)
-    val B: Tweet = Tweet("b", "b body", 20)
+    val B = Tweet("b", "b body", 20)
     val C = Tweet("c", "c body", 7)
     val D = Tweet("d", "d body", 9)
 
-    val set1 = new Empty
-    val set2 = set1.incl(A)
-    val set3 = set2.incl(B)
-    val set4c = set3.incl(C)
-    val set4d = set3.incl(D)
-    val set5 = set4c.incl(D)
+    val emptySet = new Empty
+    val setA = new Empty().incl(A)
+    val setAB = new Empty().incl(A).incl(B)
+    val setABC = new Empty().incl(A).incl(B).incl(C)
+    val setABD = new Empty().incl(A).incl(B).incl(D)
+    val setABCD = new Empty().incl(A).incl(B).incl(C).incl(D)
   }
 
   def asSet(tweets: TweetSet): Set[Tweet] = {
@@ -27,55 +27,47 @@ class TweetSetSuite extends FunSuite {
     result
   }
 
-  def fromArray(tweets: Tweet*): TweetSet = {
-    val result = new Empty
-    tweets.map(tweet => result.incl(tweet))
-    result
-  }
-
   def size(set: TweetSet): Int = asSet(set).size
 
   test("filter: on empty set") {
     new TestSets {
-      assert(size(set1.filter(tweet => tweet.user == "a")) === 0)
+      assert(size(emptySet.filter(tweet => tweet.user == "a")) === 0)
     }
   }
 
   test("filter: a on set5") {
     new TestSets {
-//      assert(size(fromArray(A, B, C, D).filter(tweet => tweet.user == "a")) === 1)
-      val result = set5.filter(tweet => tweet.user == "a")
-      assert(size(result) === 1)
+      assert(size(setABCD.filter(tweet => tweet.user == "a")) === 1)
     }
   }
 
   test("filter: 20 on set5") {
     new TestSets {
-      assert(size(set5.filter(tw => tw.retweets == 20)) === 2)
+      assert(size(setABCD.filter(tw => tw.retweets == 20)) === 2)
     }
   }
 
   test("union: set4c and set4d") {
     new TestSets {
-      assert(size(set4c.union(set4d)) === 4)
+      assert(size(setABC.union(setABD)) === 4)
     }
   }
 
   test("union: with empty set (1)") {
     new TestSets {
-      assert(size(set5.union(set1)) === 4)
+      assert(size(setABCD.union(emptySet)) === 4)
     }
   }
 
   test("union: with empty set (2)") {
     new TestSets {
-      assert(size(set1.union(set5)) === 4)
+      assert(size(emptySet.union(setABCD)) === 4)
     }
   }
 
   test("descending: set5") {
     new TestSets {
-      val trends = set5.descendingByRetweet
+      val trends = setABCD.descendingByRetweet
       assert(!trends.isEmpty)
       assert(trends.head.user == "a" || trends.head.user == "b")
     }
